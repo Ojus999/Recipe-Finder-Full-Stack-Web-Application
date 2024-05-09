0013dba54514f948f015f0422ef2f1319eb4a45c
Sathvika1064 <114856751+Sathvika1064@users.noreply.github.com>
2024-05-09 06:52:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ import Card from "./Components/Card";
 function App() {
   let [search, setSearch] = useState("");
   let [data,setData] = useState([])
+  const [userdata,setUserdata]=useState([]);
 
   function sendSearch() {
+    setUserdata([]);
     axios
       .get(`http://127.0.0.1:3001/send?data=${search}`)
       .then((res) => {        
@@ -20,7 +22,23 @@ function App() {
       })
       .catch((err) => console.log(err));
   }
-  
+  async function displayfunc(){
+    setData([]);
+    try{
+      const response = await fetch("http://localhost:3001/display", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+        });
+        const data=await response.json();
+        console.log(data.user);
+        setUserdata(data.user);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
 
   return (
     <div className="App">
@@ -36,13 +54,26 @@ function App() {
           onChange={(e) => setSearch(e.target.value)}
         />
         <img src={icon} alt="Search Icon" id="search-icon" onClick={sendSearch} />
+        
       </div>
-
+      <center><br/><br/>
+      <button onClick={displayfunc}>Display saved items</button></center>
       <div className="card--collection">
         {data.map((element,index) => {
           return <Card id = {element.id} title ={element.title} image = {element.image} key = {index}/>
         })}
       </div>
+
+      {userdata.length > 0 && (
+    <div className="card--collection">
+        {userdata.map((user) => (
+            <div >
+                <Card id={user.id} title={user.title} image={user.image} />
+            </div>
+        ))}
+    </div>
+)}
+
 
 
     </div>
