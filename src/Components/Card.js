721d9cf0cb615156dbbb708b62ba002b69277f0a
Sathvika1064 <114856751+Sathvika1064@users.noreply.github.com>
2024-05-09 06:53:03 +0530

    Add files via upload

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,31 @@
-import "../App.css"
+// App.js
 
-export default function Card({id,title,image}){
-    return(        
-        <div className="card">            
+import "../App.css";
+
+async function sendPOSTmethod({id, title, image}) {
+    try {
+        const response = await fetch("http://localhost:3001/save", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({id, title, image}),
+        });
+        const data = await response.json();
+        window.alert(data.message);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default function Card({id, title, image}) {
+    return (
+        <div className="card">
             <p><b>Recipe ID : </b>{id}</p>
             <p><b>Recipe Name:</b></p>
             <p>{title}</p>
             <img src={image} alt="Not Found" />
-            <button>Click Here</button>
+            <button onClick={() => sendPOSTmethod({id, title, image})}>Click Here to save</button>
         </div>
     );
 }
